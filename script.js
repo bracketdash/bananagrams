f11b8e61229a8efe99815bc2cf803e3ade50043c
Michael Hatch <michaelhatch@me.com>
2013-04-28 18:27:18 -0700

    Big performance imporovement
    Scrapped the permutation thing and just did some tree traversal

--- script.js
+++ script.js
@@ -16,95 +16,32 @@ var all = JSON.parse('{'+all.replace(/([a-z])/g,"\"$1\":{")
 		return '"0":1'+b+',';
 	})+'"0":1}}}}}}}}');
 
-var counts = {
-	rootExists: 0,
-	isWholeWord: 0,
-	countTest: 0
-};
-
-// check if a root exists in the dictionary
-function rootExists(members){
-	counts.rootExists++;
-	var currentMember = all;
-	for(var i = 0, ii = members.length; i < ii; i++){
-		if(currentMember[members[i]]){
-			currentMember = currentMember[members[i]];
-		} else {
-			return false;
-		}
-	}
-	return true;
-}
-
-function isWholeWord(members){
-	counts.isWholeWord++;
-	var currentMember = all;
-	for(var i = 0, ii = members.length; i < ii; i++){
-		currentMember = currentMember[members[i]];
-	}
-	if(!currentMember['0']){
-		return false;
-	}
-	return true;
-}
-
 // solve function
 function solve(string){
 	
-	var start = new Date().getTime();
-	
-	// define out vars
+	// define our vars
 	var result = [];
 	
-	// get counts for tray
-	var trayCounts = function(){
-		var i = string.length, trayCounts = {};
-		while (i){
-			trayCounts[string[--i]] = (trayCounts[string[i]] || 0) + 1;
-		}
-		return trayCounts;
-	}();
-	
-	// run a count test
-	function countTest(word){
-		counts.countTest++;
+	// the looper function
+	var count = 0;
+	function looper(object, traypart, prefix){
 		
-		// get counts for word
-		var i = word.length, wordCounts = {};
-		while (i){
-			wordCounts[word[--i]] = (wordCounts[word[i]] || 0) + 1;
-		}
+		count++;
 		
-		// run the test
-		var pass = true;
-		_.forOwn(wordCounts, function(val, key){
-			if(val > trayCounts[key]){
-				pass = false;
+		_.forOwn(object, function(val, key){
+			var traytray = traypart.slice();
+			if(key == '0'){
+				result.push(prefix);
+			}
+			if(traytray.indexOf(key) != -1){
+				traytray.splice(traytray.indexOf(key),1);
+				looper(val, traytray, prefix + key);
 			}
 		});
-		return pass;
-	}
-	
-	// the looper function
-	function loop(depth,prefix){
-		for(var i=0; i<string.length; i++){
-			var next = prefix+string[i];
-			if(rootExists(next)){
-				if(depth > 0){
-					loop(depth-1,next);
-				} else if(result.indexOf(next) == -1 && isWholeWord(next) && countTest(next)){
-					result.push(next);
-				}
-			}
-		}
 	}
 	
 	// run the looper function
-	for(var i=0; i<string.length; i++){
-		loop(i,'');
-	}
-	
-	console.log('Took '+(new Date().getTime() - start)+'ms');
+	looper(all, string.split(''), '');
 	
 	// return the word list
 	return result;
@@ -112,8 +49,8 @@ function solve(string){
 
 $(function(){
 	$('input').focus().keyup(function(e){
-		$(this).val($(this).val().replace(/[^a-z]/gi, '').toLowerCase());
 		if(e.keyCode == 13){
+			$(this).val($(this).val().replace(/[^a-z]/gi, '').toLowerCase());
 			var words = solve($(this).val()), markup = '';
 			for(var x=0,xx=words.length;x<xx;x++){
 				markup += '<div>' + words[x] + '</div>';
