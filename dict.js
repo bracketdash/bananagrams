c538db0e108a09475cff9ade8bcc6e62006e9f82
Michael Hatch <michaelhatch@reserved-thurston-2.giftservices.com>
2019-12-10 15:07:45 -0800

    update

--- dict.js
+++ dict.js
@@ -1,26 +1,26 @@
 const fs = require('fs');
 const _ = require('lodash');
 
-function getTrie() {
-    let trie = fs.readFileSync('./trie.txt', 'utf8');
-    trie = trie.replace(/([a-z])/g,"\"$1\":{");
-    trie = trie.replace(/([0-9])/g, function(num) {
-        let brackets = '';
-        _.times(parseInt(num), function() {
-            brackets += '}';
+module.exports = {
+    getTrie: function() {
+        let trie = fs.readFileSync('./trie.txt', 'utf8');
+        trie = trie.replace(/([a-z])/g,"\"$1\":{");
+        trie = trie.replace(/([0-9])/g, function(num) {
+            let brackets = '';
+            _.times(parseInt(num), function() {
+                brackets += '}';
+            });
+            return '"_":1' + brackets + ',';
         });
-		return '"_":1' + brackets + ',';
-    });
-    trie = trie.replace(/([A-Z])/g, function(num) {
-        num = num.charCodeAt() - 55;
-        let brackets = '';
-        _.times(num, function() {
-            brackets += '}';
+        trie = trie.replace(/([A-Z])/g, function(num) {
+            num = num.charCodeAt() - 55;
+            let brackets = '';
+            _.times(num, function() {
+                brackets += '}';
+            });
+            return '"_":1' + brackets + ',';
         });
-		return '"_":1' + brackets + ',';
-	});
-    trie = JSON.parse('{' + trie + '"_":1}}}}}}}}');
-    return trie;
-}
-
-console.log(getTrie());
+        trie = JSON.parse('{' + trie + '"_":1}}}}}}}}');
+        return trie;
+    }
+};
--- placer.js
+++ placer.js
@@ -1,7 +1,52 @@
 const _ = require('lodash');
 
-function placeWord(board, location, word) {
+function placeWord(board, word, row, col, dir) {
+    
+    // first resize the board if necessary
+    // TODO: abstract these out to keep things DRY
+    if (col + word.length > board[0].length) {
+        // add columns to the right
+        _.times((col + word.length) - board.length, function() {
+            let newCol = [];
+            _.times(board.length, function() {
+                newCol.push(' ');
+            });
+            board.push(newCol);
+        });
+    } else if (col < 0) {
+        // add columns to the left
+        _.times(-col, function() {
+            let newCol = [];
+            _.times(board.length, function() {
+                newCol.push(' ');
+            });
+            board.push(newCol);
+        });
+    }
+    if (row + word.length > board.length) {
+        // add rows to the bottom
+        _.times((row + word.length) - board.length, function() {
+            let newRow = [];
+            _.times(board[0].length, function() {
+                newRow.push(' ');
+            });
+            board.push(newRow);
+        });
+    } else if (row < 0) {
+        // add rows to the top
+        _.times(-row, function() {
+            let newRow = [];
+            _.times(board[0].length, function() {
+                newRow.push(' ');
+            });
+            board.unshift(newRow);
+        });
+    }
+
+    // find the first tile that needs to be edited
     // TODO
+
+    return board;
 }
 
 let board = [
@@ -12,7 +57,7 @@ let board = [
     [' ', 'm', ' '],
     [' ', 'n', ' ']
 ];
-console.log(placeWord(board, [-2, 4], 'boom'));
+console.log(placeWord(board, 'boom', 4, -2, 'row'));
 
 /*
 expected return:
