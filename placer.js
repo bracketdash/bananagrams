8bdebc4aec827a18733131ca470c5ce3f890ac13
Michael Hatch <michaelhatch@michaels-air.lan>
2019-12-10 18:40:16 -0800

    placer done

--- placer.js
+++ placer.js
@@ -1,67 +1,45 @@
 const _ = require('lodash');
 
-function placeWord(board, word, row, col, dir) {
+module.exports = {
+    placeWord: function(board, word, row, col, dir) {
 
-    let wordLen = {right:0,down:0};
-    wordLen[dir] = word.length;
+        let wordLen = {right:0,down:0};
+        wordLen[dir] = word.length;
 
-    // add rows to the top if needed
-    if (row < 0) {
-        _.times(-row, function() {
-            board.unshift(_.map(Array(board[0].length), () => ' '));
+        // add rows if needed
+        if (row < 0 || row + wordLen.down > board.length) {
+            let newRow = _.map(Array(board[0].length), () => ' ');
+            if (row < 0) {
+                _.times(-row, () => board.unshift(newRow));
+                row = 0;
+            }
+            if (row + wordLen.down > board.length) {
+                _.times(row + wordLen.down - board.length, () => board.push(newRow));
+            }
+        }
+
+        // add columns if needed
+        if (col < 0 || col + wordLen.right > board[0].length) {
+            if (col < 0) {
+                board = _.map(board, (boardRow) => _.map(Array(-col), () => ' ').concat(boardRow));
+                col = 0;
+            }
+            if (col + wordLen.right > board[0].length) {
+                board = _.map(board, (boardRow) => boardRow.concat(_.map(Array(col + wordLen.right - board[0].length), () => ' ')));
+            }
+        }
+
+        // place the letters on the board
+        board[row][col] = word[0];
+        _.forEach(word.substring(1), function(letter) {
+            if (dir === 'down') {
+                row += 1;
+            } else {
+                col += 1;
+            }
+            board[row][col] = letter;
         });
+
+        return board;
     }
-
-    // add rows to the bottom if needed
-    if (row + wordLen.down > board.length) {
-        _.times(row + wordLen.down - board.length, function() {
-            board.push(_.map(Array(board[0].length), () => ' '));
-        });
-    }
-
-    // add columns to the left if needed
-    if (col < 0) {
-        board = _.map(board, function(boardRow) {
-            return _.map(Array(-col), () => ' ').concat(boardRow);
-        });
-    }
-
-    // add columns to the right if needed
-    if (col + wordLen.right > board[0].length) {
-        board = _.map(board, function(boardRow) {
-            return boardRow.concat(_.map(Array(col + wordLen.right - board[0].length), () => ' '))
-        });
-    }
-
-    // find the first tile that needs to be edited
-    // TODO
-
-    return board;
-}
-
-let board = [
-    [' ', 'c', ' '],
-    ['r', 'o', 'w'],
-    [' ', 'l', ' '],
-    [' ', 'u', ' '],
-    [' ', 'm', ' '],
-    [' ', 'n', ' ']
-];
-
-board = placeWord(board, 'boom', 4, -2, 'right');
-
-/*
-expected return:
-[
-    [' ', ' ', ' ', 'c', ' '],
-    [' ', ' ', 'r', 'o', 'w'],
-    [' ', ' ', ' ', 'l', ' '],
-    [' ', ' ', ' ', 'u', ' '],
-    ['b', 'o', 'o', 'm', ' '],
-    [' ', ' ', ' ', 'n', ' ']
-];
-*/
-
-board = placeWord(board, 'wonder', 1, 2, 'down');
-
-console.log(board);
+};
