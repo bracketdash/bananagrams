65b544014a04bc6f62050584c85638f226b75c13
Michael Hatch <michaelhatch@michaels-air.lan>
2019-12-10 19:14:34 -0800

    start on app logic

--- app.js
+++ app.js
@@ -1,32 +1,58 @@
-/*
-bananagrams solver
+const _ = require('lodash');
 
-input:
-    string letters
-    [string] disallowed_words
-output:
-    [[char]] solution
+const combinator = require('./combinator.js');
+const dict = require('./dict.js');
+const placer = require('./placer.js');
 
-program:
+function solve(letters, disallowedWords) {
+    combinator.makeWordsWith(dict.getTrie(), letters).then(function(words) {
+        if (disallowedWords) {
+            words = _.difference(words, disallowedWords);
+        }
+        if (words.length) {
+            words = words.sort(function(a, b) {
+                if (a.length > b.length) {
+                    return -1;
+                } else if (a.length < b.length) {
+                    return 1;
+                } else {
+                    return 0;
+                }
+            });
+            // next line referred to as PLACEMENT in comments below
+            board = placer.placeWord([[]], words[0], 0, 0, 'right');
+            if (letters.length - words[0].length > 0) {
 
-[combinator.js] find words that can be made with the letters
-if words can be found
-    (PLACEMENT) [placer.js] place the longest word on the board
-    if there are letters left
-        for each row and column on the board
-            [matcher.js] find words that could fit
-        if words can be found
-            GOTO PLACEMENT AND CONTINUE
-        if words could not be found
-            remove the last placed word from the board
-            GOTO PLACEMENT
-                if there  are more words that we haven't tried
-                    place the next word in the set on the board
-                    CONTINUE FROM NEXT LINE
-                if there are no more words left to try
-                    EXIT (NO SOLUTION)
-    if there are no letters left
-        EXIT WITH SOLUTION
-if no words could be found
-    EXIT (NO SOLUTION)
-*/
+                /*
+                for each row and column on the board
+                    [matcher.js] find words that could fit
+                remove disallowed words
+                if words can be found
+                    GOTO PLACEMENT AND CONTINUE
+                if words could not be found
+                    remove the last placed word from the board
+                    GOTO PLACEMENT
+                        if there  are more words that we haven't tried
+                            place the next word in the set on the board
+                            CONTINUE FROM NEXT LINE
+                        if there are no more words left to try
+                            EXIT (NO SOLUTION)
+                */
+
+                // print board for testing
+                _.forEach(board, function(boardRow) {
+                    console.log(boardRow.join(' '));
+                });
+            } else {
+                console.log('SOLUTION FOUND');
+                _.forEach(board, function(boardRow) {
+                    console.log(boardRow.join(' '));
+                });
+            }
+        } else {
+            console.log('NO SOLUTION');
+        }
+    });
+}
+
+solve('letters', ['letters']);
