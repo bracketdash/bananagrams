4177452f0f4e1776df19affc340869a699f3af59
Michael Hatch <michaelhatch@michaels-air.lan>
2020-01-04 11:37:56 -0800

    update

--- app.js
+++ app.js
@@ -1,38 +1,33 @@
 var trie = {};
 
 var app = new Vue({
-    el: '#app',
+    el: "#app",
     data: {
-        blacklist: '',
+        blacklist: "",
         board: [[]],
-        letters: '',
-        message: '',
-        tray: ''
+        letters: "",
+        message: "",
+        tray: ""
     },
     methods: {
         solve: function() {
             var self = this;
-            this.letters = this.letters.replace(/[^A-Z]/gi, '');
-            this.blacklist = this.blacklist.replace(/[^,A-Z]/gi, '');
-            this.message = '';
-            solve(
-                self.letters,
-                self.blacklist.split(','),
-                trie,
-                function(clientState) {
-                    if (clientState.end) {
-                        self.message = clientState.message;
-                    } else if (
-                        self.letters != clientState.originalLetters ||
-                        self.blacklist != clientState.blacklist.join(',')
-                    ) {
-                        return false;
-                    }
-                    self.board = clientState.board;
-                    self.tray = clientState.tray;
-                    return true;
+            this.letters = this.letters.replace(/[^A-Z]/gi, "");
+            this.blacklist = this.blacklist.replace(/[^,A-Z]/gi, "");
+            this.message = "";
+            solve(self.letters, self.blacklist.split(","), trie, function(clientState) {
+                if (clientState.end) {
+                    self.message = clientState.message;
+                } else if (
+                    self.letters != clientState.originalLetters ||
+                    self.blacklist != clientState.blacklist.join(",")
+                ) {
+                    return false;
                 }
-            );
+                self.board = clientState.board;
+                self.tray = clientState.tray;
+                return true;
+            });
         }
     },
     mounted: function() {
--- solver.js
+++ solver.js
@@ -20,13 +20,13 @@ function getPatternLoop(fullPattern, patterns, leftTrim, rightTrim) {
     var moddedPattern = fullPattern;
     _.times(leftTrim, function() {
         if (/[a-z]+[^a-z]+[a-z]+/.test(moddedPattern)) {
-            moddedPattern = moddedPattern.replace(/^[^a-z]*[a-z]+/, '');
+            moddedPattern = moddedPattern.replace(/^[^a-z]*[a-z]+/, "");
             moddedPattern = moddedPattern.replace(/^\.\{([0-9]*)\}/, function(match, captured) {
                 var num = parseInt(captured);
                 if (num < 2) {
-                    return '';
+                    return "";
                 }
-                return '.{0,' + (num-1) + '}';
+                return ".{0," + (num - 1) + "}";
             });
         } else {
             allDone = true;
@@ -34,47 +34,50 @@ function getPatternLoop(fullPattern, patterns, leftTrim, rightTrim) {
     });
     _.times(rightTrim, function() {
         if (/[a-z]+[^a-z]+[a-z]+/.test(moddedPattern)) {
-            moddedPattern = moddedPattern.replace(/[a-z]+[^a-z]*$/, '');
+            moddedPattern = moddedPattern.replace(/[a-z]+[^a-z]*$/, "");
             moddedPattern = moddedPattern.replace(/\.\{([0-9]*)\}$/, function(match, captured) {
                 var num = parseInt(captured);
                 if (num < 2) {
-                    return '';
+                    return "";
                 }
-                return '.{0,' + (num-1) + '}';
+                return ".{0," + (num - 1) + "}";
             });
         } else {
             needsLeftTrimIteration = true;
         }
     });
     if (leftTrim > 0) {
-        moddedPattern = '^' + moddedPattern;
+        moddedPattern = "^" + moddedPattern;
     }
     if (rightTrim > 0) {
-        moddedPattern = moddedPattern + '$';
+        moddedPattern = moddedPattern + "$";
     }
     if (allDone) {
         return patterns;
     }
     if (needsLeftTrimIteration) {
-        return getPatternLoop(fullPattern, patterns, leftTrim+1, 0);
+        return getPatternLoop(fullPattern, patterns, leftTrim + 1, 0);
     } else {
         patterns.push(moddedPattern);
     }
-    return getPatternLoop(fullPattern, patterns, leftTrim, rightTrim+1);
+    return getPatternLoop(fullPattern, patterns, leftTrim, rightTrim + 1);
 }
 
 function getPattern(stripArr) {
-    var fullPattern = '.*' + _.trim(stripArr.join('')).replace(/\s+/g, function(match) {
-        return '.{' + match.length + '}';
-    }) + '.*';
-    return new RegExp(getPatternLoop(fullPattern, [fullPattern], 0, 1).join('|'));
+    var fullPattern =
+        ".*" +
+        _.trim(stripArr.join("")).replace(/\s+/g, function(match) {
+            return ".{" + match.length + "}";
+        }) +
+        ".*";
+    return new RegExp(getPatternLoop(fullPattern, [fullPattern], 0, 1).join("|"));
 }
 
 function narrowWordsBy(wordlist, letters) {
     return _.filter(wordlist, function(word) {
-       var lettersLeft = letters;
-        _.forEach(word.split(''), function(letter) {
-            lettersLeft = lettersLeft.replace(letter, '');
+        var lettersLeft = letters;
+        _.forEach(word.split(""), function(letter) {
+            lettersLeft = lettersLeft.replace(letter, "");
         });
         if (letters.length - lettersLeft.length != word.length) {
             return false;
@@ -85,10 +88,10 @@ function narrowWordsBy(wordlist, letters) {
 
 function getIndexOfWordInStripLoop(pattern, word, strip, index) {
     var spliced = _.clone(strip);
-    if (index === 'first') {
-        index = -word.length+1;
+    if (index === "first") {
+        index = -word.length + 1;
         _.some(strip, function(tile) {
-            if (tile === ' ') {
+            if (tile === " ") {
                 index += 1;
             } else {
                 return true;
@@ -96,30 +99,30 @@ function getIndexOfWordInStripLoop(pattern, word, strip, index) {
         });
     }
     if (index < 0) {
-        Array.prototype.splice.apply(spliced, [0, word.length+index].concat(word));
+        Array.prototype.splice.apply(spliced, [0, word.length + index].concat(word));
     } else {
         Array.prototype.splice.apply(spliced, [index, word.length].concat(word));
     }
-    if (pattern.test(spliced.join(''))) {
-        if (spliced.join('') === strip.join('')) {
+    if (pattern.test(spliced.join(""))) {
+        if (spliced.join("") === strip.join("")) {
             return false;
         }
         return index;
     } else {
-        return getIndexOfWordInStripLoop(pattern, word, strip, index+1);
+        return getIndexOfWordInStripLoop(pattern, word, strip, index + 1);
     }
 }
 
 function getMatchesLoop(strip, stripdex, dir, notDir, letters, wordlist, matches, callback) {
     var pattern;
-    var stripStr = strip.join('');
+    var stripStr = strip.join("");
     var stripStrTrimmed = _.trim(stripStr);
     if (!stripStrTrimmed) {
         return;
     }
     pattern = getPattern(strip);
-    _.forEach(stripStrTrimmed.split(''), function(tileOnBoard, tileIndex) {
-        if (tileOnBoard !== ' ') {
+    _.forEach(stripStrTrimmed.split(""), function(tileOnBoard, tileIndex) {
+        if (tileOnBoard !== " ") {
             var words = narrowWordsBy(wordlist, letters + tileOnBoard);
             _.forEach(words, function(word) {
                 if (pattern.test(word)) {
@@ -128,7 +131,12 @@ function getMatchesLoop(strip, stripdex, dir, notDir, letters, wordlist, matches
                         dir: dir
                     };
                     stripMatch[dir] = stripdex;
-                    stripMatch[notDir] = getIndexOfWordInStripLoop(new RegExp(stripStr.replace(/\s/g, '.')), word.split(''), strip, 'first');
+                    stripMatch[notDir] = getIndexOfWordInStripLoop(
+                        new RegExp(stripStr.replace(/\s/g, ".")),
+                        word.split(""),
+                        strip,
+                        "first"
+                    );
                     if (stripMatch[notDir] !== false) {
                         matches.push(stripMatch);
                     }
@@ -143,50 +151,58 @@ function getMatchesLoop(strip, stripdex, dir, notDir, letters, wordlist, matches
 
 function getMatches(letters, board, wordlist, resolve) {
     var matches = [];
-    crawlBoard(board, function(boardRow, boardRowIndex) {
-        getMatchesLoop(boardRow, boardRowIndex, 'row', 'col', letters, wordlist, matches);
-    }, function(boardColumn, boardColumnIndex) {
-        getMatchesLoop(boardColumn, boardColumnIndex, 'col', 'row', letters, wordlist, matches, function() {
-            if (boardColumnIndex === board[0].length-1) {
-                matches = _.sortBy(matches, function(match) {
-                    var points = 100;
-                    if (match.row > 0 && match.row < board.length) {
-                        points -= 1;
-                        if (match.dir === 'col' && match.row + match.word.length < board.length) {
-                            points -= 2;
+    crawlBoard(
+        board,
+        function(boardRow, boardRowIndex) {
+            getMatchesLoop(boardRow, boardRowIndex, "row", "col", letters, wordlist, matches);
+        },
+        function(boardColumn, boardColumnIndex) {
+            getMatchesLoop(boardColumn, boardColumnIndex, "col", "row", letters, wordlist, matches, function() {
+                if (boardColumnIndex === board[0].length - 1) {
+                    matches = _.sortBy(matches, function(match) {
+                        var points = 100;
+                        if (match.row > 0 && match.row < board.length) {
+                            points -= 1;
+                            if (match.dir === "col" && match.row + match.word.length < board.length) {
+                                points -= 2;
+                            }
                         }
-                    }
-                    if (match.col > 0 && match.col < board[0].length) {
-                        points -= 1;
-                        if (match.dir === 'row' && match.col + match.word.length < board[0].length) {
-                            points -= 2;
+                        if (match.col > 0 && match.col < board[0].length) {
+                            points -= 1;
+                            if (match.dir === "row" && match.col + match.word.length < board[0].length) {
+                                points -= 2;
+                            }
                         }
-                    }
-                    return points;
-                });
-                resolve(matches);
-            }
-        });
-    });
+                        return points;
+                    });
+                    resolve(matches);
+                }
+            });
+        }
+    );
 }
 
 function isBoardValid(board, trie, blacklist) {
     var valid = true;
-    crawlBoard(board, function(boardRow) {
-        var words = _.split(_.trim(boardRow.join('')), /\s+/);
-        _.forEach(words, function(word) {
-            if (word.length > 1 && (!_.has(trie, (word + '_').split('')) || _.indexOf(blacklist, word) > -1)) {
-                valid = false;
-            }
-        });
-    }, function(boardColumn) {
-        var words = _.split(_.trim(boardColumn.join('')), /\s+/);
-        _.forEach(words, function(word) {
-            if (word.length > 1 && (!_.has(trie, (word + '_').split('')) || _.indexOf(blacklist, word) > -1)) {
-                valid = false;
-            }
-        });
-    });
+    crawlBoard(
+        board,
+        function(boardRow) {
+            var words = _.split(_.trim(boardRow.join("")), /\s+/);
+            _.forEach(words, function(word) {
+                if (word.length > 1 && (!_.has(trie, (word + "_").split("")) || _.indexOf(blacklist, word) > -1)) {
+                    valid = false;
+                }
+            });
+        },
+        function(boardColumn) {
+            var words = _.split(_.trim(boardColumn.join("")), /\s+/);
+            _.forEach(words, function(word) {
+                if (word.length > 1 && (!_.has(trie, (word + "_").split("")) || _.indexOf(blacklist, word) > -1)) {
+                    valid = false;
+                }
+            });
+        }
+    );
     return valid;
 }
 
@@ -196,10 +212,10 @@ function placeWord(oldBoard, match) {
     var row = match.row;
     var col = match.col;
     var dir = match.dir;
-    var wordLen = {row:0,col:0};
+    var wordLen = { row: 0, col: 0 };
     wordLen[dir] = word.length;
     if (row < 0 || row + wordLen.col > board.length) {
-        var newRow = _.map(Array(board[0].length), () => ' ');
+        var newRow = _.map(Array(board[0].length), () => " ");
         if (row < 0) {
             _.times(-row, () => board.unshift(_.clone(newRow)));
             row = 0;
@@ -210,16 +226,16 @@ function placeWord(oldBoard, match) {
     }
     if (col < 0 || col + wordLen.row > board[0].length) {
         if (col < 0) {
-            board = _.map(board, (boardRow) => _.map(Array(-col), () => ' ').concat(boardRow));
+            board = _.map(board, boardRow => _.map(Array(-col), () => " ").concat(boardRow));
             col = 0;
         }
         if (col + wordLen.row > board[0].length) {
-            board = _.map(board, (boardRow) => boardRow.concat(_.map(Array(col + wordLen.row - board[0].length), () => ' ')));
+            board = _.map(board, boardRow => boardRow.concat(_.map(Array(col + wordLen.row - board[0].length), () => " ")));
         }
     }
     board[row][col] = word[0];
     _.forEach(word.substring(1), function(letter) {
-        if (dir === 'col') {
+        if (dir === "col") {
             row += 1;
         } else {
             col += 1;
@@ -233,7 +249,7 @@ function getNewLetters(incomingLetters, oldBoard, newBoard, match) {
     let newLetters = incomingLetters;
     var newStrip = [];
     var oldStrip = [];
-    if (match.dir === 'row') {
+    if (match.dir === "row") {
         newStrip = newBoard[match.row];
         oldStrip = oldBoard[match.row];
     } else {
@@ -246,7 +262,7 @@ function getNewLetters(incomingLetters, oldBoard, newBoard, match) {
     }
     let lettersToBeRemoved = _.difference(newStrip, oldStrip);
     _.forEach(lettersToBeRemoved, function(letterToBeRemoved) {
-        newLetters = newLetters.replace(letterToBeRemoved, '');
+        newLetters = newLetters.replace(letterToBeRemoved, "");
     });
     return newLetters;
 }
@@ -256,25 +272,27 @@ function solveLoop(solveState) {
     var currentMatch = currentState.matches[currentState.matchIndex];
     var newBoard;
     var newLetters;
-    if (!solveState.callback({
-        blacklist: solveState.blacklist,
-        board: currentState.board,
-        originalLetters: solveState.originalLetters,
-        tray: currentState.letters
-    })) {
+    if (
+        !solveState.callback({
+            blacklist: solveState.blacklist,
+            board: currentState.board,
+            originalLetters: solveState.originalLetters,
+            tray: currentState.letters
+        })
+    ) {
         return;
     }
     if (!currentMatch) {
         if (solveState.historyIndex > 0) {
             solveState.historyIndex -= 1;
             solveState.history[solveState.historyIndex].matchIndex += 1;
-            solveState.history = solveState.history.slice(0,-1);
+            solveState.history = solveState.history.slice(0, -1);
             setTimeout(function() {
                 solveLoop(solveState);
             });
         } else {
             solveState.callback({
-                message: 'No possible solution. Wait for a pull.',
+                message: "No possible solution. Wait for a pull.",
                 board: currentState.board,
                 tray: currentState.letters,
                 end: true
@@ -313,16 +331,16 @@ function solveLoop(solveState) {
         });
     } else {
         solveState.callback({
-            message: 'SOLVED!',
+            message: "SOLVED!",
             board: newBoard,
-            tray: '',
+            tray: "",
             end: true
         });
     }
 }
 
 function makeWordsWithLoop(branch, letters, prefix, words, resolve) {
-    var newPrefix = '';
+    var newPrefix = "";
     var lastLeaf = true;
     _.forEach(_.uniq(letters), function(letter) {
         if (!!branch[letter]) {
@@ -330,10 +348,10 @@ function makeWordsWithLoop(branch, letters, prefix, words, resolve) {
                 lastLeaf = false;
             }
             newPrefix = prefix + letter;
-            if (branch[letter]['_']) {
+            if (branch[letter]["_"]) {
                 words.push(newPrefix);
             }
-            makeWordsWithLoop(branch[letter], _.replace(letters, letter, ''), newPrefix, words, resolve);
+            makeWordsWithLoop(branch[letter], _.replace(letters, letter, ""), newPrefix, words, resolve);
         }
     });
     if (lastLeaf) {
@@ -343,7 +361,7 @@ function makeWordsWithLoop(branch, letters, prefix, words, resolve) {
 
 function makeWordsWith(letters, trie, disallowedWords) {
     var words = [];
-    makeWordsWithLoop(trie, letters, '', [], function(result) {
+    makeWordsWithLoop(trie, letters, "", [], function(result) {
         words = result;
     });
     if (disallowedWords) {
@@ -355,21 +373,27 @@ function makeWordsWith(letters, trie, disallowedWords) {
 function solve(incomingLetters, blacklist, trie, callback) {
     var board = [[]];
     var letters = incomingLetters.toLowerCase();
-    var words = makeWordsWith(letters, trie, _.map(blacklist, function(disallowedWord) {
-        return disallowedWord.toLowerCase();
-    }));
+    var words = makeWordsWith(
+        letters,
+        trie,
+        _.map(blacklist, function(disallowedWord) {
+            return disallowedWord.toLowerCase();
+        })
+    );
     if (words.length) {
         solveLoop({
             blacklist: blacklist,
             callback: callback,
-            history: [{
-                board: board,
-                letters: letters,
-                matches: _.map(words, function(word) {
-                    return { word: word, dir: 'row', row: 0, col: 0 };
-                }),
-                matchIndex: 0
-            }],
+            history: [
+                {
+                    board: board,
+                    letters: letters,
+                    matches: _.map(words, function(word) {
+                        return { word: word, dir: "row", row: 0, col: 0 };
+                    }),
+                    matchIndex: 0
+                }
+            ],
             historyIndex: 0,
             originalLetters: incomingLetters,
             trie: trie,
@@ -377,7 +401,7 @@ function solve(incomingLetters, blacklist, trie, callback) {
         });
     } else {
         callback({
-            message: 'No possible solution. Wait for a pull.',
+            message: "No possible solution. Wait for a pull.",
             board: board,
             tray: letters,
             end: true
