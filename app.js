b3bc994e5eadc47103ba86a9c6111404ed377a05
Michael Hatch <michaelhatch@me.com>
2020-12-02 19:48:19 -0800

    Update app.js

--- app.js
+++ app.js
@@ -3,14 +3,19 @@
 TODO:
 
 - make a new project using create-react-app
-- redesign to make status messages prominent until a solution is found
-- maybe turn dictionary and board into classes?
-- maybe use a compressed version of the dictionary that unpacks on load?
-- store board state as a rows Map() and a columns Map() (i.e. `rows: new Map([ [1, { start: 2, tiles: "c-t" }] ])`)
-- store state history as a Map() (i.e. `history: new Map([ [1, boardState ] ])`)
+- redesign ui to make status messages prominent until a solution is found
+
+- turn dictionary into a class (separate file)
 - store the word list as a Map() (i.e. `words: new Map([ [1, "word" ] ])`)
 - create a new Map() of (alphabetized tray letters) => Set(indexes that reference words that can be made with the tray letters)
 - create a new Map() of (`${position of letter in word}:${letter}`) => Set(indexes that reference words with the provided letter at the given position)
+- maybe make a version that uses a compressed dictionary that unpacks on load?
+
+- turn board into a class (separate file)
+- store board state as a `rows` Map() and a `columns` Map()
+    - (i.e. `rows: new Map([ [1, new Set([`${position of letter in word}:${letter}`])], .. ])`)
+- store state history as a Map() (i.e. `history: new Map([ [1, boardState] ])`)
+
 - use the changes above to optimize the solver for speed; goals:
     - >80% of solutions in <0.4s
     - >95% of solutions in <1.0s
