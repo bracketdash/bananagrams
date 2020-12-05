1ed422e9b45f72872c31c2b5aae03040fa4c3e20
Michael Hatch <michaelhatch@me.com>
2020-12-05 14:16:39 -0800

    Update trie.js

--- src/trie.js
+++ src/trie.js
@@ -1,32 +1,24 @@
 import { fromAlphaCode } from "./alpha";
 import words from "./words.txt";
 
-const parseSymbols = function(t) {
-  const reSymbol = new RegExp('([0-9A-Z]+):([0-9A-Z]+)');
-  for (let i = 0; i < t.nodes.length; i++) {
-    const m = reSymbol.exec(t.nodes[i]);
-    if (!m) {
-      t.symCount = i;
-      break;
-    }
-    t.syms[fromAlphaCode(m[1])] = fromAlphaCode(m[2]);
-  }
-  t.nodes = t.nodes.slice(t.symCount, t.nodes.length);
-}
-
-const unpack = function(str) {
-  const trie = {
-    nodes: str.split(';'),
-    syms: [],
-    symCount: 0,
-  }
-  parseSymbols(trie);
-  return trie;
-}
-
 class Trie {
   constructor() {
-    this.trie = unpack(words);
+    const trie = {
+      nodes: str.split(';'),
+      syms: [],
+      symCount: 0,
+    }
+    const pattern = new RegExp('([0-9A-Z]+):([0-9A-Z]+)');
+    trie.nodes.forEach((node, i) => {
+      const m = pattern.exec(node);
+      if (!m) {
+        trie.symCount = i;
+        break;
+      }
+      trie.syms[fromAlphaCode(m[1])] = fromAlphaCode(m[2]);
+    });
+    trie.nodes = trie.nodes.slice(trie.symCount, trie.nodes.length);
+    this.trie = trie;
   }
 
   getMap() {
