f80b0b4923bb789738af289f8430658986eb2ac8
Michael Hatch <michaelhatch@me.com>
2020-12-04 23:20:23 -0800

    Update and rename compressedWords.js to trie.js

--- src/compressedWords.js
+++
@@ -1,3 +0,0 @@
-export default {
-  "len2": "abad...",
-};
---
+++ src/trie.js
@@ -0,0 +1,3 @@
+// TODO: get a fresh word list, aim for an official bananagrams list
+// TODO: import word list into a trie, and compress it
+// TODO: save the compressed trie in this file along with the necessary code to convert it into a trie that can be used in the WordList class
