c625a682573cac1914aec41886bcf85460fadb71
Michael Hatch <michaelhatch@me.com>
2020-12-04 23:52:29 -0800

    Update trie.js

--- src/trie.js
+++ src/trie.js
@@ -1,3 +1,14 @@
-// TODO: get a fresh word list, aim for an official bananagrams list
-// TODO: import word list into a trie, and compress it
-// TODO: save the compressed trie in this file along with the necessary code to convert it into a trie that can be used in the WordList class
+class Trie {
+  constructor() {
+    const compressed = "TODO: compressed trie";
+    // TODO: uncompress `compressed`
+    this.trie = new Map();
+    // TODO: populate `this.trie`
+  }
+  
+  getMap() {
+    return this.trie;
+  }
+}
+
+export const createTrie = () => new Trie();
