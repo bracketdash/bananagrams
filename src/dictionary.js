6b58c44bcfa3cbbb91a6366ffa180647989a4bd3
Michael Hatch <michaelhatch@me.com>
2020-12-05 13:59:25 -0800

    Update dictionary.js

--- src/dictionary.js
+++ src/dictionary.js
@@ -1,5 +1,39 @@
 import { createTrie } from "./trie";
 
+/* Example of how to read/traverse the trie:
+
+const toArray = function(trie) {
+  const all = []
+  const crawl = (index, pref) => {
+    let node = trie.nodes[index]
+    if (node[0] === '!') {
+      all.push(pref)
+      node = node.slice(1) //ok, we tried. remove it.
+    }
+    const matches = node.split(/([A-Z0-9,]+)/g)
+    for (let i = 0; i < matches.length; i += 2) {
+      const str = matches[i]
+      const ref = matches[i + 1]
+      if (!str) {
+        continue
+      }
+
+      const have = pref + str
+      //branch's end
+      if (ref === ',' || ref === undefined) {
+        all.push(have)
+        continue
+      }
+      const newIndex = indexFromRef(trie, ref, index)
+      crawl(newIndex, have)
+    }
+  }
+  crawl(0, '')
+  return all
+}
+
+*/
+
 class Dictionary {
   constructor() {
     const trie = createTrie();
