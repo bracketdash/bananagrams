c189446eb79a6dd8d68f22269039dde3952d5a39
Michael Hatch <michael.hatch@gifttree.com>
2020-12-05 12:50:03 -0800

    update

--- src/trie.js
+++ src/trie.js
@@ -3,6 +3,9 @@ class Trie {
     const compressed = "TODO: compressed trie";
     // TODO: uncompress `compressed`
     // to get array of words: Object.keys(efrt.unpack(packd));
+    // would be nice if we can somehow extract the trie itself from words.txt instead of using efrt.unpack
+    // -- one less dependency
+    // -- we can get the trie itself instead of getting the array of words
     this.trie = new Map();
     // TODO: populate `this.trie`
   }
