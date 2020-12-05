c95611decce30ad730f45615ee9da44eab801a55
Michael Hatch <michaelhatch@me.com>
2020-12-05 13:09:35 -0800

    Update trie.js

--- src/trie.js
+++ src/trie.js
@@ -4,7 +4,7 @@ class Trie {
   constructor() {
     this.trie = new Map();
     // TODO: reverse engineer `efrt.unpack` to unpack JUST the trie from `words` (instead of unpacking to an array of words)
-    // TODO: we can probably get rid of the leading "true" in words.txt
+    // TODO: note - we got rid of the leading "true" in words.txt
     // TODO: instead of unpacking into a normal object-based ({}) trie, populate this.trie
   }
 
