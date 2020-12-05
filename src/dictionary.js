4dc08ba6d349acb5cdb8cbb88efc2d38196eb535
Michael Hatch <michaelhatch@me.com>
2020-12-05 00:19:47 -0800

    Update dictionary.js

--- src/dictionary.js
+++ src/dictionary.js
@@ -1,24 +1,24 @@
 import { createTrie } from "./trie";
 
-const has = function (trieMap, key) {
-  return !!trieMap && (key.length > 1 ? has(trieMap.get(key[0]), key.slice(1)) : trieMap.has(key));
-};
-
-const trie = createTrie();
-const trieMap = trie.getMap();
-
 class Dictionary {
-  constructor() {}
+  constructor() {
+    const trie = createTrie();
+    this.trie = trie.getMap();
+  }
   
   getPossiblePlacements({ tray, rowSegments, colSegments }) {
     // TODO
   }
+  
+  has(trie, key) {
+    return !!trie && (key.length > 1 ? has(trie.get(key[0]), key.slice(1)) : trie.has(key));
+  }
+  
+  isAWord(str) {
+    const chars = str.split("");
+    chars.push("_");
+    return this.has(trieMap, chars);
+  }
 }
 
 export const createDictionary = () => new Dictionary();
-
-export const isAWord = (str) => {
-  const chars = str.split("");
-  chars.push("_");
-  return has(trieMap, chars);
-};
