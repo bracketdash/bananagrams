d2a27054b8a98bc18f8fae397ace85f85fa19d74
Michael Hatch <michaelhatch@me.com>
2020-12-11 02:14:21 -0800

    Update board.js

--- src/board.js
+++ src/board.js
@@ -1,20 +1,27 @@
 class Board {
-  constructor(rows) {
-    this.rows = new Map();
-    rows.forEach((cells, row) => {
-      const cloneCells = new Map();
-      cells.forEach((cell, col) => {
-        cloneCells.set(col, cell);
-      });
-      this.rows.set(row, cloneCells);
-    });
-  }
-  getNext() {
-    // TODO
+  constructor({ numCols, numRows, rows }) {
+    this.numCols = numCols || 1;
+    this.numRows = numRows || 1;
+    this.rows = rows || new Map([ [0, new Map([ [0, " "] ])] ]);
   }
   getArray() {
-    // TODO
+    return [...Array(this.numRows).keys()].map((rowIndex) => {
+      const row = this.board.get(rowIndex);
+      const columns = Array(this.numCols).fill(" ");
+      if (row) {
+        row.forEach((col, colIndex) => {
+          columns[colIndex] = col;
+        });
+      }
+      return columns;
+    });
+  }
+  getNext(placement) {
+    let numColumns = 1;
+    // TODO: apply the placement
+    const numRows = Math.max(...rows.keys()) + 1;
+    return new Board({ numCols, numRows, rows });
   }
 }
 
-export const createBoard = (rows) => new Board(rows);
+export const createBoard = (config) => new Board(config);
