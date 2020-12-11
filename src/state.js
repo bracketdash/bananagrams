53f3d1aa61b3e5dac991ead6b55a1197c944e6c0
Michael Hatch <michaelhatch@me.com>
2020-12-11 02:40:26 -0800

    Update state.js

--- src/state.js
+++ src/state.js
@@ -1,11 +1,5 @@
 import { createPlacement } from "./placement";
 
-// State is instantiated in: solve.js (Solve.start)
-// The following methods are called in solve.js (Solve.solve): getAdvanced, getNext, getPrev, isSolved
-// The following methods are called in solve.js (Solve.update): getBoard, getTray
-// All other methods are only used in this file
-// It represents a state of the solve and provides state traversal functionality
-
 class State {
   constructor({ blacklist, board, parent, placement, tray, trie }) {
     this.blacklist = blacklist;
