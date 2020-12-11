2ccf45e54d43f6a55e16a9b7abe900e86eefe650
Michael Hatch <michaelhatch@me.com>
2020-12-10 21:48:11 -0800

    Update state.js

--- src/state.js
+++ src/state.js
@@ -1,12 +1,33 @@
+// State is instantiated in: solve.js (Solve.start)
+// The following methods are called in solver.js (Solver.solve): getAdvanced, getNext, getPrev, isSolved
+// The following methods are called in solver.js (Solver.update): getBoard, getTray
+// All other methods are only used in this file
+// It represents a possible state of the solve and provides state traversal functionality
+
 class State {
-  constructor({ board, tray }) {
+  constructor({ blacklist, board, tray }) {
+    this.blacklist = blacklist;
     this.board = board;
     this.tray = tray;
+  }
+  getAdvanced() {
+    // TODO
+  }
+  getBoard() {
+    // TODO
   }
   getNext() {
     // TODO
   }
-  // TODO
+  getPrev() {
+    // TODO
+  }
+  getTray() {
+    // TODO
+  }
+  isSolved() {
+    return this.tray.isEmpty();
+  }
 }
 
 export const createState = (config) => new State(config);
