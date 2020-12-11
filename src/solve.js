0fcca0b1855f0bdcf0e73e8790b1300e742b4563
Michael Hatch <michaelhatch@me.com>
2020-12-10 21:20:31 -0800

    Update solve.js

--- src/solve.js
+++ src/solve.js
@@ -4,9 +4,46 @@ import { createState } from "./state";
 class Solve {
   constructor({ blacklist, tray, trie }) {
     this.blacklist = blacklist;
-    this.states = new Map();
     this.tray = tray;
     this.trie = trie;
+  }
+  onUpdate(updateFn) {
+    this.updateFn = updateFn;
+  }
+  start() {
+    this.start = new Date().getTime();
+    this.step(createState({
+      blacklist: this.blacklist,
+      board: createBoard(new Map()),
+      tray: this.trie,
+    }));
+    return this.start;
+  }
+  step(state) {
+    if (state.isSolved()) {
+      this.update(state, "Solution found!");
+      return;
+    }
+    if (!this.tryNextStep(state.getAdvanced(), "Advancing state...")) {
+      if (!this.tryNextStep(state.getNext(), "Trying next state...")) {
+        let prevState = state.getPrev();
+        while (prevState) {
+          if (this.tryNextStep(prevState.getNext(), "Trying previous next state...") {
+            return;
+          }
+          prevState = prevState.getPrev();
+        }
+        this.update(state, "No solutions possible!");
+      }
+    }
+  }
+  tryNextStep(maybeNextState, message) {
+    if (!maybeNextState) {
+      return false;
+    }
+    setTimeout(() => this.step(nextState));
+    this.update(nextState, message);
+    return true;
   }
   update(state, message) {
     return this.updateFn({
@@ -14,58 +51,6 @@ class Solve {
       message,
       tray: state.getTray(),
     }, this.start);
-  }
-  onUpdate(updateFn) {
-    this.updateFn = updateFn;
-  }
-  start() {
-    const now = new Date().getTime();
-    this.start = now;
-    this.states.set("0", createState({
-      board: createBoard(new Map()),
-      tray: this.trie,
-    }));
-    this.step("0");
-    return now;
-  }
-  step(key) {
-    const state = this.states.get(key);
-    if (state.isSolved()) {
-      this.update(state, "Solution found!");
-      return;
-    }
-    let newKey = "";
-    const advancedState = state.getAdvanced();
-    if (advancedState) {
-      // TODO: 3:5 => 3:5:0
-      // TODO: take the next step with this state; call this.step(newKey)
-      this.update(advancedState, "Advancing state...");
-      return;
-    }
-    const nextState = state.getNext();
-    if (nextState) {
-      // TODO: 3:5:0 => 3:5:1
-      // TODO: take the next step with this state; call this.step(newKey)
-      this.update(nextState, "Trying next state...");
-      return;
-    }
-    let prevState = state.getPrev();
-    let prevNextState = false;
-    while (!prevNextState) {
-      prevNextState = prevState.getNext();
-      if (prevNextState) {
-        // TODO: assign newKey --  3:5:1 => 3:6
-        this.states.set(newKey, prevNextState);
-        this.step(newKey);
-        this.update(prevNextState, "Trying previous next state...");
-        return;
-      }
-      if (/* there are no colons left in the newKey */) {
-        // TODO: update with no solutions posssible
-        return;
-      }
-      prevState = prevState.getPrev();
-    }
   }
 }
 
