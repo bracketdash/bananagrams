fa42d4eb6286a44a819ab7b86ae9f22f7cd57a9d
Michael Hatch <michaelhatch@me.com>
2020-12-10 23:31:20 -0800

    Update placement.js

--- src/placement.js
+++ src/placement.js
@@ -1,18 +1,37 @@
+import { createSegment } from "./segment";
+import { createWord } from "./word";
+
 class Placement {
-  constructor({ blacklist, board, tray, trie }) {
+  constructor({ blacklist, board, segment, tray, trie, word }) {
     this.blacklist = blacklist;
     this.board = board;
+    this.segment = segment;
     this.tray = tray;
     this.trie = trie;
+    this.word = word;
   }
   getNext() {
-    // TODO
+    // TODO: create the next placement for the current word and segment
+    // TODO: if no placement can be made, try making the first placement for the next word for the current segment
+    // TODO: if no placement can be made, try making the first placement for the first word for the next segment
+    // TODO: if we're also out of segments, return false;
   }
   getPlacedTiles() {
-    // TODO
+    // TODO: return an array of letters that would be added to the board
   }
   init() {
-    // TODO
+    const { blacklist, board, tray, trie } = this;
+    const segment = createSegment({ board });
+    if (!segment) {
+      return false;
+    }
+    const word = createWord({ blacklist, segment, tray, trie });
+    if (!word) {
+      return false;
+    }
+    this.segment = segment;
+    this.word = word;
+    // TODO: create the first placement
   }
 }
 
