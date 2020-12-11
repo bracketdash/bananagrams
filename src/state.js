22074a2a3608eb4ac45feda040054faf73ddc1fe
Michael Hatch <michaelhatch@me.com>
2020-12-10 16:46:58 -0800

    Update state.js

--- src/state.js
+++ src/state.js
@@ -1,3 +1,11 @@
+/*
+State(solve[, config])
+  Creator: Solve (internal)
+  Methods:
+    getBoard() => Board (board)
+    getNext(placement) => State (nextState)
+*/
+
 class State {
   constructor(config) {
     // TODO
@@ -7,4 +15,5 @@ class State {
   }
   // TODO
 }
+
 export const createState = (config) => new State(config);
