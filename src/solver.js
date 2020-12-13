cbba7532a6c35fd9f691bb86d9995e42f35d5eef
Michael Hatch <michaelhatch@me.com>
2020-12-13 15:04:56 -0800

    Update solver.js

--- src/solver.js
+++ src/solver.js
@@ -3,6 +3,11 @@ import { createSolve } from "./solve";
 import { createTray } from "./tray";
 import { createTrie } from "./trie";
 
+// Instantiated once per app
+// Creates and starts solves
+// Makes sure only one solve is happening at a time
+// Handles updates coming up from solves
+
 class Solver {
   constructor() {
     this.solveStart = 0;
