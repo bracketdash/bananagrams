2bcd0bd6cafcf37df3da89277d53a46a17aa7c02
Michael Hatch <michaelhatch@me.com>
2020-12-13 17:17:34 -0800

    Update solver.js

--- src/solver.js
+++ src/solver.js
@@ -16,36 +16,22 @@ class Solver {
       this.updateFn({ ready: true });
     });
   }
-  handleRawUpdate({ board, message, tray }) {
-    const update = {};
-    if (board) {
-      update.boardArr = board.getArray();
-    }
-    if (message) {
-      update.message = message;
-    }
-    if (tray) {
-      update.remainingTray = tray.getString();
-    }
-    this.updateFn(update);
-  }
   onUpdate(updateFn) {
     this.updateFn = updateFn;
   }
   solve({ blacklistStr, trayStr }) {
-    const solve = createSolve({
+    this.solveStart = createSolve({
       blacklist: createBlacklist(blacklistStr),
+      solver: this,
       tray: createTray(trayStr),
-      trie: this.trie,
-    });
-    solve.onUpdate((rawUpdate, start) => {
-      if (start !== this.solveStart) {
-        return false;
+      update: (update, start) => {
+        if (start !== this.solveStart) {
+          return false;
+        }
+        this.updateFn(update);
+        return true;
       }
-      this.handleRawUpdate(rawUpdate);
-      return true;
     });
-    this.solveStart = solve.start();
   }
 }
 
