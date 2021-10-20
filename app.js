2394dcde9938205ce26a2096dd06c66721b893ef
Michael Hatch <michaelhatch@me.com>
2021-10-20 16:12:10 -0700

    prettier

--- app.js
+++ app.js
@@ -1,36 +1,41 @@
 var trie = {};
 
 var app = new Vue({
-    el: "#app",
-    data: {
-        blacklist: "",
-        board: [[]],
-        letters: "",
-        message: "",
-        tray: ""
-    },
-    methods: {
-        solve: function() {
-            var self = this;
-            this.letters = this.letters.replace(/[^A-Z]/gi, "");
-            this.blacklist = this.blacklist.replace(/[^,A-Z]/gi, "");
-            this.message = "";
-            solve(self.letters, self.blacklist.split(","), trie, function(clientState) {
-                if (clientState.end) {
-                    self.message = clientState.message;
-                } else if (
-                    self.letters != clientState.originalLetters ||
-                    self.blacklist != clientState.blacklist.join(",")
-                ) {
-                    return false;
-                }
-                self.board = clientState.board;
-                self.tray = clientState.tray;
-                return true;
-            });
+  el: "#app",
+  data: {
+    blacklist: "",
+    board: [[]],
+    letters: "",
+    message: "",
+    tray: "",
+  },
+  methods: {
+    solve: function () {
+      var self = this;
+      this.letters = this.letters.replace(/[^A-Z]/gi, "");
+      this.blacklist = this.blacklist.replace(/[^,A-Z]/gi, "");
+      this.message = "";
+      solve(
+        self.letters,
+        self.blacklist.split(","),
+        trie,
+        function (clientState) {
+          if (clientState.end) {
+            self.message = clientState.message;
+          } else if (
+            self.letters != clientState.originalLetters ||
+            self.blacklist != clientState.blacklist.join(",")
+          ) {
+            return false;
+          }
+          self.board = clientState.board;
+          self.tray = clientState.tray;
+          return true;
         }
+      );
     },
-    mounted: function() {
-        trie = getTrie();
-    }
+  },
+  mounted: function () {
+    trie = getTrie();
+  },
 });
