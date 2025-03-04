e77f982fe1c3cf190cc4810e8c0475c9a1039313
Michael Hatch <michaelhatch@me.com>
2025-03-04 11:50:43 -0800

    optimize app.js

--- app.js
+++ app.js
@@ -5,42 +5,6 @@ const boardBox = document.getElementById("board");
 const trayBox = document.getElementById("tray");
 const messageBox = document.getElementById("message");
 
-function printBoard(board) {
-  let boardHtml = "";
-  board.forEach((row) => {
-    boardHtml += '<div class="row">';
-    row.forEach((cell) => {
-      boardHtml += `<div class="cell${
-        cell === " " ? " empty" : ""
-      }">${cell}</div>`;
-    });
-    boardHtml += "</div>";
-  });
-  boardBox.innerHTML = boardHtml;
-}
-
-function handleSolveTick({
-  blacklist,
-  board,
-  currBlacklist,
-  currLetters,
-  end,
-  message,
-  originalLetters,
-  tray,
-}) {
-  if (
-    !end &&
-    (currLetters != originalLetters || currBlacklist != blacklist.join(","))
-  ) {
-    return false;
-  }
-  printBoard(board);
-  trayBox.innerHTML = tray;
-  messageBox.innerHTML = end ? message : "";
-  return true;
-}
-
 const trie = JSON.parse(
   "{" +
     compressedTrie
@@ -52,12 +16,37 @@ const trie = JSON.parse(
 function solveBoard() {
   const currLetters = tilesInput.value.replace(/[^A-Z]/gi, "");
   tilesInput.value = currLetters;
-
   const currBlacklist = blacklistInput.value.replace(/[^,A-Z]/gi, "");
   blacklistInput.value = currBlacklist;
-
-  solve(currLetters, currBlacklist.split(","), trie, (state) =>
-    handleSolveTick({ ...state, currLetters, currBlacklist })
+  solve(
+    currLetters,
+    currBlacklist.split(","),
+    trie,
+    ({ blacklist, board, end, message, originalLetters, tray }) => {
+      if (
+        !end &&
+        (currLetters != originalLetters || currBlacklist != blacklist.join(","))
+      ) {
+        return false;
+      }
+      boardBox.innerHTML = board
+        .map(
+          (row) =>
+            `<div class="row">${row
+              .map(
+                (cell) =>
+                  `<div class="cell${
+                    cell === " " ? " empty" : ""
+                  }">${cell}</div>`
+              )
+              .join("")}
+        </div>`
+        )
+        .join("");
+      trayBox.innerHTML = tray;
+      messageBox.innerHTML = end ? message : "";
+      return true;
+    }
   );
 }
 
